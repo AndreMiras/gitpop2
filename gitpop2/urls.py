9389cc4a4b7961550e46974204dafc067826f2b6
Andre Miras <andre.miras@gmail.com>
2014-03-01 11:31:01 +0100

    improved repo url pattern to also match dash

--- gitpop2/urls.py
+++ gitpop2/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'gitpop2.views.home', name='home'),
-    url(r'^pop/(?P<owner>\w+)/(?P<repo>\w+)/$', 'gitpop2.views.repo_pop', name='repo_pop'),
+    url(r'^pop/(?P<owner>[-\w]+)/(?P<repo>[-\w]+)/$', 'gitpop2.views.repo_pop', name='repo_pop'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
