54bc2d2844909d5e3014e0aee82d25083ca7797d
Andre Miras <andre.miras@gmail.com>
2014-03-02 00:25:33 +0100

    serving static files in prod, refs https://devcenter.heroku.com/articles/getting-started-with-django#wsgi-py

--- gitpop2/wsgi.py
+++ gitpop2/wsgi.py
@@ -10,5 +10,11 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gitpop2.settings")
 
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+
+# https://devcenter.heroku.com/articles/getting-started-with-django#wsgi-py
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
