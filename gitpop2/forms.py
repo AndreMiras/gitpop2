7f70003b76f84963252a54194290a42a522a3d55
Andre Miras <andre.miras@gmail.com>
2014-03-01 15:13:18 +0100

    placeholder and wider git repo form

--- gitpop2/forms.py
+++ gitpop2/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 class PopForm(forms.Form):
     # url = forms.CharField()
-    giturl = forms.URLField()
+    giturl = forms.URLField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'https://github.com/django/django',
+            'class': 'input-xlarge',
+            }))
