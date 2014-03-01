120432391895a5af18bb5adb71fcc2e93f83e182
Andre Miras <andre.miras@gmail.com>
2014-03-02 00:53:12 +0100

    using env variables for production secret keys

--- gitpop2/prod_settings.py
+++ gitpop2/prod_settings.py
@@ -3,6 +3,8 @@ from base_settings import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
