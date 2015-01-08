0eea1668e9cbd90ba524e0f68f75b256ae85d8d2
Andre Miras <andre.miras@gmail.com>
2015-01-08 19:34:45 +0100

    more sexy (shorter) pop url

--- gitpop2/urls.py
+++ gitpop2/urls.py
@@ -4,12 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'gitpop2.views.home', name='home'),
-    url(r'^pop/(?P<owner>[-\w.]+)/(?P<repo>[-\w.]+)/$', 'gitpop2.views.repo_pop', name='repo_pop'),
     url(r'^pop_form/$', 'gitpop2.views.pop_form', name='pop_form'),
     url(r'^contact/$', 'gitpop2.views.contact', name='contact'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^(?P<owner>[-\w.]+)/(?P<repo>[-\w.]+)$', 'gitpop2.views.repo_pop', name='repo_pop'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- gitpop2/views.py
+++ gitpop2/views.py
@@ -19,7 +19,6 @@ def home(request):
 
 def pop_form(request):
     if request.method == 'POST': # If the form has been submitted...
-        # ContactForm was defined in the the previous section
         form = PopForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
             # Process the data in form.cleaned_data
