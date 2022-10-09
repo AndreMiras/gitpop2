2d563ae5ce2be2294c359aaa49e5747051466b6a
Andre Miras <andre.miras@gmail.com>
2022-10-09 22:25:27 +0200

    :heavy_minus_sign: Drop DB dependency
    The project doesn't need any database setup.

--- gitpop2/base_settings.py
+++ gitpop2/base_settings.py
@@ -79,12 +79,7 @@ WSGI_APPLICATION = "gitpop2.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+DATABASES = {}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
--- gitpop2/prod_settings.py
+++ gitpop2/prod_settings.py
@@ -1,17 +1,10 @@
 import os
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-
-from .base_settings import *  # noqa: F403 unable to detect undefined names
+from .base_settings import *  # noqa: F401, F403
 
 DEBUG = False
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-
-DATABASES[  # noqa: F405 'DATABASES' may be undefined
-    "default"
-] = dj_database_url.config()
 
 ADMINS = ((os.environ["ADMIN_NAME"], os.environ["ADMIN_EMAIL"]),)
 
